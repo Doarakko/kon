7631c6f7e9cf0c71b5d3c92a0d90a2a2cd96e8df
peperoncino <2wua4nlyi@gmail.com>
2023-01-19 01:01:55 +0900

    kong

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use image::load_from_memory;
+use std::collections::HashMap;
+use viuer::{print, Config};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://randomfox.ca/floof/")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    let img_bytes = reqwest::get(&resp["image"]).await?.bytes().await?;
+    let img = load_from_memory(&img_bytes)?;
+
+    let conf = Config {
+        y: img.height() as i16,
+        ..Default::default()
+    };
+    print(&img, &conf)?;
+
+    Ok(())
+}
